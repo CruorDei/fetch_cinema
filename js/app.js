2337072f0183bcc59ce508e01ebdb3314073d387
CruorDei <baptiste.denoue@gmail.com>
2023-12-15 14:52:24 +0100

    Votre message de commit ici

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// app.js
 import CinemaFinder from './cinemaFinder.js';
-import { getUserPosition } from './geolocation.js';
+import { getUserPosition, getAddressFromCoordinates, getCoordinatesFromAddress } from './geolocation.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const cinemaFinder = new CinemaFinder();
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     } catch (error) {
         console.error('Erreur lors de la récupération de la position :', error);
     }
-    
+
     async function refreshCinemaList(userPosition) {
         try {
             const sortMode = sortModeSelect.value;
@@ -28,49 +29,73 @@ document.addEventListener('DOMContentLoaded', async () => {
             console.error('Erreur lors de la récupération des cinémas :', error);
         }
     }
-});
 
-function displaySortMode(container, sortMode) {
-    container.textContent = `Mode de tri actuel : ${sortMode === 'fauteuils' ? 'Par nombre de fauteuils' : 'Par distance'}`;
-}
+    function displaySortMode(container, sortMode) {
+        container.textContent = `Mode de tri actuel : ${sortMode === 'fauteuils' ? 'Par nombre de fauteuils' : 'Par distance'}`;
+    }
 
-function displayCinemaTable(container, cinemas, userPosition) {
-    container.innerHTML = '';
+    function displayCinemaTable(container, cinemas, userPosition) {
+        container.innerHTML = '';
 
-    const table = document.createElement('table');
-    table.classList.add('cinemaTable');
+        const table = document.createElement('table');
+        table.classList.add('cinemaTable');
 
-    const headerRow = document.createElement('tr');
-    headerRow.innerHTML = `
-        <th>Nom</th>
-        <th>Adresse</th>
-        <th>Commune</th>
-        <th>Fauteuils</th>
-        <th>Séances</th>
-        <th>Entrees 2022</th>
-        <th>Genre</th>
-        <th>Latitude</th>
-        <th>Longitude</th>
-        <th>Distance (km)</th>
-    `;
-    table.appendChild(headerRow);
-
-    cinemas.forEach(cinema => {
-        const cinemaRow = document.createElement('tr');
-        cinemaRow.innerHTML = `
-            <td>${cinema.nom}</td>
-            <td>${cinema.adresse}</td>
-            <td>${cinema.commune}</td>
-            <td>${cinema.fauteuils}</td>
-            <td>${cinema.seances}</td>
-            <td>${cinema.entrees_2022}</td>
-            <td>${cinema.genre}</td>
-            <td>${cinema.geolocalisation.lat}</td>
-            <td>${cinema.geolocalisation.lon}</td>
-            <td>${cinema.distance.toFixed(2)}</td>
+        const headerRow = document.createElement('tr');
+        headerRow.innerHTML = `
+            <th>Nom</th>
+            <th>Adresse</th>
+            <th>Commune</th>
+            <th>Fauteuils</th>
+            <th>Séances</th>
+            <th>Entrees 2022</th>
+            <th>Genre</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Distance (km)</th>
         `;
-        table.appendChild(cinemaRow);
+        table.appendChild(headerRow);
+
+        cinemas.forEach(cinema => {
+            const cinemaRow = document.createElement('tr');
+            cinemaRow.innerHTML = `
+                <td>${cinema.nom}</td>
+                <td>${cinema.adresse}</td>
+                <td>${cinema.commune}</td>
+                <td>${cinema.fauteuils}</td>
+                <td>${cinema.seances}</td>
+                <td>${cinema.entrees_2022}</td>
+                <td>${cinema.genre}</td>
+                <td>${cinema.geolocalisation.lat}</td>
+                <td>${cinema.geolocalisation.lon}</td>
+                <td>${cinema.distance.toFixed(2)}</td>
+            `;
+            table.appendChild(cinemaRow);
+        });
+
+        container.appendChild(table);
+    }
+
+    /*etape 3*/
+
+    document.getElementById('geolocateButton').addEventListener('click', async () => {
+        try {
+            const userPosition = await getUserPosition();
+            const address = await getAddressFromCoordinates(userPosition.lat, userPosition.lon);
+            document.getElementById('addressInput').value = address;
+        } catch (error) {
+            console.error('Erreur lors de la géolocalisation :', error);
+        }
     });
 
-    container.appendChild(table);
-}
+    document.getElementById('searchForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const address = document.getElementById('addressInput').value;
+        try {
+            const coordinates = await getCoordinatesFromAddress(address);
+            const cinemas = await cinemaFinder.getCinemasNearUser(10, 'fauteuils', coordinates);
+            displayCinemaTable(cinemaListContainer, cinemas, coordinates);
+        } catch (error) {
+            console.error('Erreur lors de la recherche des cinémas :', error);
+        }
+    });
+});
--- js/geolocation.js
+++ js/geolocation.js
@@ -1,3 +1,4 @@
+// geolocation.js
 export async function getUserPosition() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
@@ -7,13 +8,28 @@ export async function getUserPosition() {
     });
 }
 
-export async function getAdministrativeZone(lat, lon) {
+export async function getAddressFromCoordinates(lat, lon) {
     const apiUrl = `https://api-adresse.data.gouv.fr/reverse/?lon=${lon}&lat=${lat}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     if (data && data.features && data.features.length > 0) {
-        return data.features[0].properties.context;
+        return data.features[0].properties.label;
+    } else {
+        return null;
+    }
+}
+
+export async function getCoordinatesFromAddress(address) {
+    const apiUrl = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(address)}&type=housenumber&autocomplete=1`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data && data.length > 0) {
+        return {
+            lat: data[0].y,
+            lon: data[0].x
+        };
     } else {
         return null;
     }
